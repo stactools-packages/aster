037faa2c5d4a155202bf2fd4af04cda4d29be570
Tyler Battle <tyler@battle.ca>
2021-06-11 22:13:03 -0700

    Move test external-data declaration from main library

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,13 @@
 from stactools.testing import TestData
 
-test_data = TestData(__file__)
+_external_data = {
+    'AST_L1T_00305032000040446_20150409135350_78838.hdf': {
+        'url':
+        ('https://ai4epublictestdata.blob.core.windows.net/'
+         'stactools/aster/AST_L1T_00305032000040446_20150409135350_78838.zip'),
+        'compress':
+        'zip'
+    }
+}
+
+test_data = TestData(__file__, _external_data)
